7abaf787c94d9e557c8ccac2d5c76b44d7878433
Hanzala Bin Khalid <hanzalabinkhalid@gmail.com>
2026-08-05 15:36:52 +0500

    Configure OpenNext for Cloudflare

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://migvpurparvqaytzpran.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pZ3ZwdXJwYXJ2cWF5dHpwcmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4Mzk4MjYsImV4cCI6MjEwMTQxNTgyNn0.O9S-bLmfpa4_VS8QsEDdkZSCp9chyp4_a30qjAmkSU8"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://migvpurparvqaytzpran.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pZ3ZwdXJwYXJ2cWF5dHpwcmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4Mzk4MjYsImV4cCI6MjEwMTQxNTgyNn0.O9S-bLmfpa4_VS8QsEDdkZSCp9chyp4_a30qjAmkSU8"};
+export const test = {};
